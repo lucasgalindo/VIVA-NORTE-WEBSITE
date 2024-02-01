445eaf7b046ae63087eb8ab4e6feacaeea18da64
aluno <aluno@embarquedigital.portodigital.local>
2024-02-01 11:51:03 -0300

    +addons

--- VIVA-NORTE-WEBSITE/src/componentes/ModalPhotos/ModalPhotos.jsx
+++ VIVA-NORTE-WEBSITE/src/componentes/ModalPhotos/ModalPhotos.jsx
@@ -3,10 +3,13 @@
 export default function ModalPhotos({data}){
     return(
         <div className="fullscreen-container">
-            {data.pictures.map(({url}, index) =>{
+            
+            
+            
+            {/* {data.pictures.map(({url}, index) =>{
                 console.log(url)
                 return <img src={url} alt="" />
-            })}
+            })} */}
         </div>
     );
 }
